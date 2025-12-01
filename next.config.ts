179dd1b982a2e8d2e93918c724203fb1b8e1f4ab
Nathan Caudeli <nathan.caudeli@hotmail.com>
2025-12-01 12:58:46 +0100

    wip : add showcase

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   turbopack: {
+    root: __dirname,
     resolveAlias: {
       '@': './src',
       '@public': './public',
